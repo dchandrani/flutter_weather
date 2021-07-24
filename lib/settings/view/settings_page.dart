40f740bbc5d05a8b4a33ed7cac17afa3faedc0a5
Dhruv Chandrani <dhruv.chandrani@gmail.com>
2021-07-25 01:36:31 +0530

    Completed the app

---
+++ lib/settings/view/settings_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_weather/weather/weather.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({Key? key}) : super(key: key);
+
+  static Route route(WeatherCubit weatherCubit) {
+    return MaterialPageRoute<void>(
+      builder: (_) => BlocProvider.value(
+        value: weatherCubit,
+        child: SettingsPage(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: ListView(
+        children: [
+          BlocBuilder<WeatherCubit, WeatherState>(
+            buildWhen: (previous, current) =>
+                previous.temperatureUnits != current.temperatureUnits,
+            builder: (context, state) {
+              return ListTile(
+                title: const Text('Tempratur Units'),
+                isThreeLine: true,
+                subtitle: const Text(
+                    'Use metric measurements for temperature units.'),
+                trailing: Switch(
+                  value: state.temperatureUnits.isCelsius,
+                  onChanged: (value) =>
+                      context.read<WeatherCubit>().toggleUnits(),
+                ),
+              );
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
--- packages/weather_repository/test/weather_repository_test.dart
+++ packages/weather_repository/test/weather_repository_test.dart
@@ -1,5 +1,1 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:weather_repository/weather_repository.dart';
-
 void main() {}
